b365fe3831121e08111b9809ae429322aa4977f1
earth-tk <hi05yu24earth41@gmail.com>
2026-06-22 03:29:41 +0900

    いろいろかえる

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,37 +1,26 @@
-$(function(){
-  
+function initCursor() {
   //カーソル要素の指定
-  var cursor=$("#cursor");
-  
-  //mousemoveイベントでカーソル要素を移動させる
-  $(document).on("mousemove",function(e){
-               //カーソルの座標位置を取得
-    var x=e.clientX;
-    var y=e.clientY;
-    //カーソル要素のcssを書き換える用
-    cursor.css({
-      "opacity":"1",
-      "top":y+"px",
-      "left":x+"px"
-    });
-  });
-});
+  var cursor = $("#cursor");
+  var cursorSub = $("#cursor_sub");
 
-$(function(){
-  
-  //カーソル要素の指定
-  var cursor=$("#cursor_sub");
-  
   //mousemoveイベントでカーソル要素を移動させる
-  $(document).on("mousemove",function(e){
-               //カーソルの座標位置を取得
-    var x=e.clientX;
-    var y=e.clientY;
+  $(document).on("mousemove", function(e) {
+    //カーソルの座標位置を取得
+    var x = e.clientX;
+    var y = e.clientY;
     //カーソル要素のcssを書き換える用
     cursor.css({
-      "opacity":"1",
-      "top":y+"px",
-      "left":x+"px"
+      "opacity": "1",
+      "top": y + "px",
+      "left": x + "px"
+    });
+    cursorSub.css({
+      "opacity": "1",
+      "top": y + "px",
+      "left": x + "px"
     });
   });
-});
+}
+
+// common.js実行後に初期化
+window.addEventListener('load', initCursor);
